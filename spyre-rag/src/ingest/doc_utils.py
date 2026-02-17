dharaneeshvrd/project-ai-services

commit fb7ceab642e624c5140327836cebec6f08f974b6

Handle pdfs with 0 table (#318)
Move process text & table to a separate method to make it cleaner

---------

Signed-off-by: Dharaneeshwaran Ravichandran <dharaneeshwaran.ravichandran@ibm.com>

spyre-rag/src/ingest/doc_utils.py
@@ -65,6 +65,128 @@ def get_doc_converter():
 
     return doc_converter
 
+def process_text(converted_doc, pdf_path, out_path):
+    page_count = 0
+    process_time = 0.0
+
+    # Initialize TocHeaders to get the Table of Contents (TOC)
+    t0 = time.time()
+    toc_headers = None
+    try:
+        toc_headers, page_count = get_toc(pdf_path)
+    except Exception as e:
+        logger.debug(f"No TOC found or failed to load TOC: {e}")
+
+    # Load pdf pages one time when TOC headers not found for retrieving the font size of header texts
+    pdf_pages = None
+    if not toc_headers:
+        pdf_pages = load_pdf_pages(pdf_path)
+        page_count = len(pdf_pages)
+
+    # --- Text Extraction ---
+    if not converted_doc.texts:
+        logger.debug(f"No text content found in '{pdf_path}'")
+        out_path.write_text(json.dumps([], indent=2), encoding="utf-8")
+        return page_count, process_time
+
+    structured_output = []
+    last_header_level = 0
+    for text_obj in tqdm_wrapper(converted_doc.texts, desc=f"Processing text content of '{pdf_path}'"):
+        label = text_obj.label
+        if label in excluded_labels:
+            continue
+
+        # Check if it's a section header and process TOC or fallback to font size extraction
+        if label == "section_header":
+            prov_list = text_obj.prov
+
+            for prov in prov_list:
+                page_no = prov.page_no
+
+                if toc_headers:
+                    header_prefix = get_matching_header_lvl(toc_headers, text_obj.text)
+                    if header_prefix:
+                        # If TOC matches, use the level from TOC
+                        structured_output.append({
+                            "label": label,
+                            "text": f"{header_prefix} {text_obj.text}",
+                            "page": page_no,
+                            "font_size": None,  # Font size isn't necessary if TOC matches
+                        })
+                        last_header_level = len(header_prefix.strip())  # Update last header level
+                    else:
+                        # If no match, use the previous header level + 1
+                        new_header_level = last_header_level + 1
+                        structured_output.append({
+                            "label": label,
+                            "text": f"{'#' * new_header_level} {text_obj.text}",
+                            "page": page_no,
+                            "font_size": None,  # Font size isn't necessary if TOC matches
+                        })
+                else:
+                    matches = find_text_font_size(pdf_pages, text_obj.text, page_no - 1)
+                    if len(matches):
+                        font_size = 0
+                        count = 0
+                        for match in matches:
+                            font_size += match["font_size"] if match["match_score"] == 100 else 0
+                            count += 1 if match["match_score"] == 100 else 0
+                        font_size = font_size / count if count else None
+
+                        structured_output.append({
+                            "label": label,
+                            "text": text_obj.text,
+                            "page": page_no,
+                            "font_size": round(font_size, 2) if font_size else None
+                        })
+        else:
+            structured_output.append({
+                "label": label,
+                "text": text_obj.text,
+                "page": text_obj.prov[0].page_no,
+                "font_size": None
+            })
+
+        process_time = time.time() - t0
+        out_path.write_text(json.dumps(structured_output, indent=2), encoding="utf-8")
+        
+    return page_count, process_time
+
+def process_table(converted_doc, pdf_path, out_path, gen_model, gen_endpoint):
+    table_count = 0
+    process_time = 0.0
+    filtered_table_dicts = {}
+    t0 = time.time()
+    # --- Table Extraction ---
+    if not converted_doc.tables:
+        logger.debug(f"No tables found in '{pdf_path}'")
+        out_path.write_text(json.dumps({}, indent=2), encoding="utf-8")
+        return table_count, process_time
+    
+    table_dict = {}
+    for table_ix, table in enumerate(tqdm_wrapper(converted_doc.tables, desc=f"Processing table content of '{pdf_path}'")):
+        table_dict[table_ix] = {}
+        table_dict[table_ix]["html"] = table.export_to_html(doc=converted_doc)
+        table_dict[table_ix]["caption"] = table.caption_text(doc=converted_doc)
+
+    table_htmls = [table_dict[key]["html"] for key in sorted(table_dict)]
+    table_captions_list = [table_dict[key]["caption"] for key in sorted(table_dict)]
+
+    table_summaries, decisions = summarize_and_classify_tables(table_htmls, gen_model, gen_endpoint, pdf_path)
+    filtered_table_dicts = {
+        idx: {
+            'html': html,
+            'caption': caption,
+            'summary': summary
+        }
+        for idx, (keep, html, caption, summary) in enumerate(zip(decisions, table_htmls, table_captions_list, table_summaries)) if keep
+    }
+    table_count = len(filtered_table_dicts)
+    out_path.write_text(json.dumps(filtered_table_dicts, indent=2), encoding="utf-8")
+    process_time = time.time() - t0
+
+    return table_count, process_time
+
 def process_converted_document(converted_json_path, pdf_path, out_path, conversion_stats, gen_model, gen_endpoint, emb_endpoint, max_tokens):    
     stem = Path(pdf_path).stem
     processed_text_json_path = (Path(out_path) / f"{stem}{text_suffix}")
@@ -89,110 +211,12 @@ def process_converted_document(converted_json_path, pdf_path, out_path, conversi
             raise Exception(f"failed to load converted json into Docling Document")
 
         if not conversion_stats["text_processed"]:
+            page_count, process_time = process_text(converted_doc, pdf_path, processed_text_json_path)
+            timings["process_text"] = process_time
 
-            # Initialize TocHeaders to get the Table of Contents (TOC)
-            t0 = time.time()
-            toc_headers = None
-            try:
-                toc_headers, page_count = get_toc(pdf_path)
-            except Exception as e:
-                logger.debug(f"No TOC found or failed to load TOC: {e}")
-
-            # Load pdf pages one time when TOC headers not found for retrieving the font size of header texts
-            pdf_pages = None
-            if not toc_headers:
-                pdf_pages = load_pdf_pages(pdf_path)
-                page_count = len(pdf_pages)
-
-            # --- Text Extraction ---
-            structured_output = []
-            last_header_level = 0
-            for text_obj in tqdm_wrapper(converted_doc.texts, desc=f"Processing text content of '{pdf_path}'"):
-                label = text_obj.label
-                if label in excluded_labels:
-                    continue
-
-                # Check if it's a section header and process TOC or fallback to font size extraction
-                if label == "section_header":
-                    prov_list = text_obj.prov
-
-                    for prov in prov_list:
-                        page_no = prov.page_no
-
-                        if toc_headers:
-                            header_prefix = get_matching_header_lvl(toc_headers, text_obj.text)
-                            if header_prefix:
-                                # If TOC matches, use the level from TOC
-                                structured_output.append({
-                                    "label": label,
-                                    "text": f"{header_prefix} {text_obj.text}",
-                                    "page": page_no,
-                                    "font_size": None,  # Font size isn't necessary if TOC matches
-                                })
-                                last_header_level = len(header_prefix.strip())  # Update last header level
-                            else:
-                                # If no match, use the previous header level + 1
-                                new_header_level = last_header_level + 1
-                                structured_output.append({
-                                    "label": label,
-                                    "text": f"{'#' * new_header_level} {text_obj.text}",
-                                    "page": page_no,
-                                    "font_size": None,  # Font size isn't necessary if TOC matches
-                                })
-                        else:
-                            matches = find_text_font_size(pdf_pages, text_obj.text, page_no - 1)
-                            if len(matches):
-                                font_size = 0
-                                count = 0
-                                for match in matches:
-                                    font_size += match["font_size"] if match["match_score"] == 100 else 0
-                                    count += 1 if match["match_score"] == 100 else 0
-                                font_size = font_size / count if count else None
-
-                                structured_output.append({
-                                    "label": label,
-                                    "text": text_obj.text,
-                                    "page": page_no,
-                                    "font_size": round(font_size, 2) if font_size else None
-                                })
-                else:
-                    structured_output.append({
-                        "label": label,
-                        "text": text_obj.text,
-                        "page": text_obj.prov[0].page_no,
-                        "font_size": None
-                    })
-
-            timings["process_text"] = time.time() - t0
-
-            processed_text_json_path.write_text(json.dumps(structured_output, indent=2), encoding="utf-8")
-            
         if not conversion_stats["table_processed"]:
-            filtered_table_dicts = {}
-            t0 = time.time()
-            # --- Table Extraction ---
-            if converted_doc.tables:
-                table_dict = {}
-                for table_ix, table in enumerate(tqdm_wrapper(converted_doc.tables, desc=f"Processing table content of '{pdf_path}'")):
-                    table_dict[table_ix] = {}
-                    table_dict[table_ix]["html"] = table.export_to_html(doc=converted_doc)
-                    table_dict[table_ix]["caption"] = table.caption_text(doc=converted_doc)
-
-                table_htmls = [table_dict[key]["html"] for key in sorted(table_dict)]
-                table_captions_list = [table_dict[key]["caption"] for key in sorted(table_dict)]
-
-                table_summaries, decisions = summarize_and_classify_tables(table_htmls, gen_model, gen_endpoint, pdf_path)
-                filtered_table_dicts = {
-                    idx: {
-                        'html': html,
-                        'caption': caption,
-                        'summary': summary
-                    }
-                    for idx, (keep, html, caption, summary) in enumerate(zip(decisions, table_htmls, table_captions_list, table_summaries)) if keep
-                }
-                table_count = len(filtered_table_dicts)
-                processed_table_json_path.write_text(json.dumps(filtered_table_dicts, indent=2), encoding="utf-8")
-                timings['process_tables'] = time.time() - t0
+            table_count, process_time = process_table(converted_doc, pdf_path, processed_table_json_path, gen_model, gen_endpoint)
+            timings["process_tables"] = process_time
 
         return pdf_path, processed_text_json_path, processed_table_json_path, page_count, table_count, timings
     except Exception as e:

commit 039112ebbef4062066b2a8b79bb042aef4518c00

Bug fix on processing text during ingestion (#328)
Writing to json should happen outside of text processing loop

This bug got introduced in
https://github.com/IBM/project-ai-services/pull/318
Apologies for missing it.

Signed-off-by: Dharaneeshwaran Ravichandran <dharaneeshwaran.ravichandran@ibm.com>

ai-services/assets/applications/rag-dev/podman/values.yaml
@@ -8,7 +8,7 @@ backend:
   # @description Host port for the OpenAI-compatible RAG service. Defaults to unexposed; assign a port to enable external access.
   port: "0"
   # @hidden
-  image: icr.io/ai-services-cicd/rag:v0.0.29
+  image: icr.io/ai-services-cicd/rag:v0.0.30
   # @hidden
   log_level: "INFO"
 

ai-services/assets/applications/rag/podman/values.yaml
@@ -8,7 +8,7 @@ backend:
   # @description Host port for the OpenAI-compatible RAG service. Defaults to unexposed; assign a port to enable external access.
   port: "0"
   # @hidden
-  image: icr.io/ai-services-cicd/rag:v0.0.29
+  image: icr.io/ai-services-cicd/rag:v0.0.30
   # @hidden
   log_level: "INFO"
 

spyre-rag/src/Makefile
@@ -1,6 +1,6 @@
 REGISTRY?=icr.io/ai-services-private
 IMAGE=rag
-TAG?=v0.0.29
+TAG?=v0.0.30
 CREDS_ARG := $(if $(and $(REGISTRY_USER),$(REGISTRY_PASSWORD)),--creds="$(REGISTRY_USER):$(REGISTRY_PASSWORD)")
 
 CONTAINER_BUILDER?=podman

spyre-rag/src/ingest/doc_utils.py
@@ -147,8 +147,8 @@ def process_text(converted_doc, pdf_path, out_path):
                 "font_size": None
             })
 
-        process_time = time.time() - t0
-        out_path.write_text(json.dumps(structured_output, indent=2), encoding="utf-8")
+    process_time = time.time() - t0
+    out_path.write_text(json.dumps(structured_output, indent=2), encoding="utf-8")
         
     return page_count, process_time
 